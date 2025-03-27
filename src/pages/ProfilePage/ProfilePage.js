cd4ad5f7052c0bedfb09dde1af15e60906b58582
Kevin Meisenbacher <kevinmeisenbacher@gmail.com>
2025-03-27 13:09:42 -0400

    Updated resume and profile

--- src/pages/ProfilePage/ProfilePage.js
+++ src/pages/ProfilePage/ProfilePage.js
@@ -4,50 +4,49 @@ const ProfilePage = () => {
     return (
         <main className='profile'>
             <section className='section'>
-                <p className='story'>Passion for improving lives with high quality software </p>
-                <p className='story'>Brings out everyone's unique strengths </p>
-                <p className='story'>Careful to achieve reliable results for an infinitely unique user base</p>
+                <p className='story'>Passion for improving lives with high quality software</p>
+                <p className='story'>Focused on reliable results for all demographics and needs</p>
+                <p className='story'>Inspires team synergy by recognizing individual strengths</p>
+                <p className='story'>Learns thoroughly for proper understanding to build efficient features</p>
             </section>
             <section className='section'>
                 <h2 className='title'>Skills</h2>
                 <article className='skills'>
                     <div className='skills-column'>
-                        <h3>Programming</h3>
+                        <h3>Languages</h3>
                         <p>JavaScript</p>
                         <p>TypeScript</p>
                         <p>C#</p>
+                        <p>Go</p>
+                        <p>Java</p>
+                        <p>MySQL</p>
+                        <p>SQLite</p>
+                        <p>MongoDB</p>
+                        <p>Firebase</p>
+                        <p>HTML5</p>
+                        <p>CSS3</p>
+                        <p>SASS</p>
                     </div>
                     <div className='skills-column'>
-                        <h3>Front End</h3>
-                        <p>HTML5</p>
-                        <p>CSS/SASS/TailwindCSS</p>
-                        <p>React</p>
+                        <h3>Resources</h3>
+                        <p>React.js</p>
+                        <p>Node.js</p>
+                        <p>Express.js</p>
+                        <p>Knex</p>
+                        <p>TailwindCSS</p>
+                        <p>ShadUI</p>
+                        <p>RESTful APIs</p>
                         <p>Axios</p>
                     </div>
                     <div className='skills-column'>
-                        <h3>Back End</h3>
-                        <p>Node.js</p>
-                        <p>Express.js</p>
-                        <p>RESTful APIs</p>
-                        <p>Knex.js</p>
-                        <p>OAuth</p>
-                        <p>ASP.NET</p>
-                    </div>
-                    <div className='skills-column'>
-                        <h3>Databases</h3>
-                        <p>MySQL</p>
-                        <p>SQLite</p>
-                        <p>MS SQL</p>
-                        <p>Firebase</p>
-                        <p>MongoDB</p>
-                    </div>
-                    <div className='skills-column'>
-                        <h3>Workflow/Deployment</h3>
+                        <h3>Workflow</h3>
                         <p>Git</p>
-                        <p>JIRA</p>
-                        <p>Amazon Web Services</p>
-                        <p>Vercel</p>
+                        <p>AWS</p>
                         <p>Heroku</p>
+                        <p>Vercel</p>
+                        <p>JIRA</p>
+                        <p>Miro</p>
+                        <p>Figjam</p>
                     </div>
                 </article>
             </section>
